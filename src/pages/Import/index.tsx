7d641036c2d4d42dee0630e037a7c9fa086f43dd
Kelsey Torres de Freitas Júnior <kelseytfjunior@gmail.com>
2020-04-25 15:29:59 -0300

    Add files via upload

---
+++ src/pages/Import/index.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import filesize from 'filesize';
+
+import Header from '../../components/Header';
+import FileList from '../../components/FileList';
+import Upload from '../../components/Upload';
+
+import { Container, Title, ImportFileContainer, Footer } from './styles';
+
+import alert from '../../assets/alert.svg';
+import api from '../../services/api';
+
+interface FileProps {
+  file: File;
+  name: string;
+  readableSize: string;
+}
+
+const Import: React.FC = () => {
+  const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
+  const history = useHistory();
+
+  async function handleUpload(): Promise<void> {
+    const files = uploadedFiles.map((uploadedFile: FileProps) => {
+      const data = new FormData();
+      data.set('file', uploadedFile.file);
+      return data;
+    });
+
+    try {
+      await Promise.all(
+        files.map(async (file: FormData) =>
+          api.post('/transactions/import', file),
+        ),
+      );
+      setUploadedFiles([]);
+      history.push('/');
+    } catch (err) {
+      console.log(err.response.error);
+    }
+  }
+
+  function submitFile(files: File[]): void {
+    const uploadFiles = files.map((file: File) => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(uploadFiles);
+  }
+
+  return (
+    <>
+      <Header size="small" />
+      <Container>
+        <Title>Importar uma transação</Title>
+        <ImportFileContainer>
+          <Upload onUpload={submitFile} />
+          {!!uploadedFiles.length && <FileList files={uploadedFiles} />}
+
+          <Footer>
+            <p>
+              <img src={alert} alt="Alert" />
+              Permitido apenas arquivos CSV
+            </p>
+            <button
+              onClick={handleUpload}
+              type="button"
+              disabled={!uploadedFiles.length}
+            >
+              Enviar
+            </button>
+          </Footer>
+        </ImportFileContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Import;
